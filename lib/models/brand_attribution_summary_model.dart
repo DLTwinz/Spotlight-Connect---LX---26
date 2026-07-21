68b827606ea6d766151cf2fa7baa43b34efd6d83
Ubuntu <DL_Twinz@spotlight-dev-vm.32jj3lc1yvjendcy1pq2sy5acd.cx.internal.cloudapp.net>
2026-07-21 13:00:38 +0000

    Phase 4: add missing attribution summary model stubs

--- lib/models/brand_attribution_summary_model.dart
+++ lib/models/brand_attribution_summary_model.dart
@@ -1,28 +1,6 @@
 class BrandAttributionSummary {
-  final String? brandId;
-  final String? brandName;
-  final num? totalSpend;
-  final num? avgDealValue;
-  final num? completionRatePct;
-  final String? summaryGeneratedAt;
+  final String label;
+  final num value;
 
-  const BrandAttributionSummary({
-    this.brandId,
-    this.brandName,
-    this.totalSpend,
-    this.avgDealValue,
-    this.completionRatePct,
-    this.summaryGeneratedAt,
-  });
-
-  factory BrandAttributionSummary.fromJson(Map<String, dynamic> json) {
-    return BrandAttributionSummary(
-      brandId: json['brand_id']?.toString(),
-      brandName: json['brand_name']?.toString(),
-      totalSpend: json['total_spend'] as num?,
-      avgDealValue: json['avg_deal_value'] as num?,
-      completionRatePct: json['completion_rate_pct'] as num?,
-      summaryGeneratedAt: json['summary_generated_at']?.toString(),
-    );
-  }
+  const BrandAttributionSummary({required this.label, required this.value});
 }
--- lib/models/creator_attribution_summary_model.dart
+++ lib/models/creator_attribution_summary_model.dart
@@ -1,28 +1,6 @@
 class CreatorAttributionSummary {
-  final String? creatorId;
-  final String? creatorName;
-  final num? totalEarnings;
-  final num? pipelineValue;
-  final num? completionRatePct;
-  final String? summaryGeneratedAt;
+  final String label;
+  final num value;
 
-  const CreatorAttributionSummary({
-    this.creatorId,
-    this.creatorName,
-    this.totalEarnings,
-    this.pipelineValue,
-    this.completionRatePct,
-    this.summaryGeneratedAt,
-  });
-
-  factory CreatorAttributionSummary.fromJson(Map<String, dynamic> json) {
-    return CreatorAttributionSummary(
-      creatorId: json['creator_id']?.toString(),
-      creatorName: json['creator_name']?.toString(),
-      totalEarnings: json['total_earnings'] as num?,
-      pipelineValue: json['pipeline_value'] as num?,
-      completionRatePct: json['completion_rate_pct'] as num?,
-      summaryGeneratedAt: json['summary_generated_at']?.toString(),
-    );
-  }
+  const CreatorAttributionSummary({required this.label, required this.value});
 }
